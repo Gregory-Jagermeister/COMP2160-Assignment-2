6c1d4a8ccbaa6e538745d66323765634860848bc
Daniel Stirba <daniel.stirba@students.mq.edu.au>
2020-11-07 00:26:37 +1100

    added smoothing effect to camera control

--- Comp2160 Ass2/Assets/CameraTracker.cs
+++ Comp2160 Ass2/Assets/CameraTracker.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class CameraTracker : MonoBehaviour
 {
   public Transform target;
-  public LayerMask groundLayer;
-  void LateUpdate()
+  //public LayerMask groundLayer;
+  public float smoothSpeed;
+  public Vector3 offset;
+  void FixedUpdate()
   {
     // fire a ray at the ground
-    Ray ray = new Ray(target.position, Vector3.down);
-    RaycastHit hit;
-    if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
-    {
-      // move the pivot to the point it hit
-      transform.position = hit.point;
-      Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.white);
-    }
+    // Ray ray = new Ray(target.position, Vector3.down);
+    // RaycastHit hit;
+    // if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
+    // {
+    //   // move the pivot to the point it hit
+    //   transform.position = hit.point;
+    //   Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.white);
+    // }
+
+    //transform.position = target.position;
+    // Vector3 rot = new Vector3(0, target.rotation.y, 0);
+
+    Vector3 desiredPos = target.position + offset;
+    Vector3 vel = Vector3.zero;
+    Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.fixedDeltaTime);
+
+    transform.position = smoothedPos;
+
+    transform.LookAt(target);
   }
 }
--- Comp2160 Ass2/Assets/CarMovement.cs
+++ Comp2160 Ass2/Assets/CarMovement.cs
@@ -8,15 +8,42 @@ public class CarMovement : MonoBehaviour
   // Start is called before the first frame update
   private Rigidbody rb;
   public float thrust;
+
+  public float angle;
   void Start()
   {
     rb = GetComponent<Rigidbody>();
   }
 
   // Update is called once per frame
-  void Update()
+  void FixedUpdate()
   {
+
+
     float moveDir = Input.GetAxisRaw("Vertical");
-    rb.AddForce(transform.forward * (moveDir * thrust));
+    //rb.MovePosition(transform.position + (transform.forward * (moveDir * thrust)) * Time.fixedDeltaTime);
+    rb.AddForce(transform.forward * (moveDir * thrust), ForceMode.VelocityChange);
+    //rb.velocity = new Vector3(0, rb.velocity.y, thrust * moveDir);
+
+    float rotDir = Input.GetAxisRaw("Horizontal");
+    Vector3 EulerAngleVelocity = new Vector3(0, angle, 0);
+
+    Quaternion deltaRot;
+
+    if (moveDir != 0)
+    {
+      if (moveDir > 0)
+      {
+        deltaRot = Quaternion.Euler((EulerAngleVelocity * rotDir) * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * deltaRot);
+      }
+      else
+      {
+        deltaRot = Quaternion.Euler((EulerAngleVelocity * -rotDir) * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * deltaRot);
+      }
+    }
+
+
   }
 }
