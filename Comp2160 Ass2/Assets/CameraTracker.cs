5a6d72bce9e01778df559ce98903f5a32c15ad9b
Daniel Stirba <daniel.stirba@students.mq.edu.au>
2020-11-06 14:29:26 +1100

    Got some basic scripts running.

---
+++ Comp2160 Ass2/Assets/CameraTracker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracker : MonoBehaviour
+{
+  public Transform target;
+  public LayerMask groundLayer;
+  void LateUpdate()
+  {
+    // fire a ray at the ground
+    Ray ray = new Ray(target.position, Vector3.down);
+    RaycastHit hit;
+    if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
+    {
+      // move the pivot to the point it hit
+      transform.position = hit.point;
+      Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.white);
+    }
+  }
+}
---
+++ Comp2160 Ass2/Assets/CarMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CarMovement : MonoBehaviour
+{
+  // Start is called before the first frame update
+  private Rigidbody rb;
+  public float thrust;
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float moveDir = Input.GetAxisRaw("Vertical");
+    rb.AddForce(transform.forward * (moveDir * thrust));
+  }
+}
